43baae6792dbb9356dee0e2aa436c22439566a22
Haansen <frodan1989@gmail.com>
2025-06-30 09:57:41 +0200

    dsa

--- components/junctek_ble/const.py
+++
@@ -1,5 +0,0 @@
-"""Constants for the junctek bms ble component."""
-
-
-CONF_CHARGING = "charging"
-CONF_DISCHARGING = "discharging"
--- components/junctek_ble/sensor.py
+++ components/junctek_ble/sensor.py
@@ -1,7 +1,13 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import ble_client, sensor
-from esphome.const import CONF_ID, CONF_VOLTAGE, UNIT_VOLT, ICON_FLASH, DEVICE_CLASS_VOLTAGE
+from esphome.const import (
+    CONF_ID,
+    CONF_VOLTAGE,
+    UNIT_VOLT,
+    ICON_FLASH,
+    DEVICE_CLASS_VOLTAGE,
+)
 
 CODEOWNERS = ["@yourgithub"]
 DEPENDENCIES = ["ble_client"]
@@ -12,7 +18,12 @@ JunctekBLEClient = junctek_ns.class_("JunctekBLEClient", cg.Component, ble_clien
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(JunctekBLEClient),
     cv.Required(CONF_ID): cv.use_id(ble_client.BLEClient),
-    cv.Required(CONF_VOLTAGE): sensor.sensor_schema(UNIT_VOLT, ICON_FLASH, 2, DEVICE_CLASS_VOLTAGE),
+    cv.Required(CONF_VOLTAGE): sensor.sensor_schema(
+        unit_of_measurement=UNIT_VOLT,
+        icon=ICON_FLASH,
+        accuracy_decimals=2,
+        device_class=DEVICE_CLASS_VOLTAGE,
+    ),
 }).extend(cv.COMPONENT_SCHEMA)
 
 def to_code(config):
